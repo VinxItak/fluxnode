2a3864d33fbacf14da1bb11c1d9d0fa13470e768
VinxItak <github@vinx.fr>
2026-02-06 16:23:28 +0100

    Checkpoint from VS Code for coding agent session

--- README.md
+++ README.md
@@ -1,30 +1,33 @@
 # Auto restart on local website failure
+
 Auto restart the node when local homepage is down
 
 # How it works:
-Get upnp port from hostname (ex : cumulus**2** = port 161**2**6), adapt it for your usage
 
-1. Test the homepage
+1. Get upnp port from config file
 
-  a. Test if page is up but UPNP failure is present
-  
-  b. restart the node if UPNP failure is present
+2. Test the homepage
 
-2. Wait 10m if down and test again
+a. Test if page is up but UPNP failure is present
 
-3. Restart if still down
+b. restart the node if UPNP failure is present
 
+3. Wait 10m if down and test again
+
+4. Restart if still down
 
 # How to use:
+
 Login to the server with the same user as the node (`home` directory where the flux node is installed) using `ssh`
 
 download the script
 
 ```
-wget -N https://github.com/VinxItak/fluxnode/releases/download/v2.0.1/test_flux.sh
-wget -N https://github.com/VinxItak/fluxnode/releases/download/v2.0.1/puppeteer_upnp.js
+wget -N https://github.com/VinxItak/fluxnode/releases/download/v3.0.0/test_flux.sh
+wget -N https://github.com/VinxItak/fluxnode/releases/download/v3.0.0/puppeteer_upnp.js
 
 ```
+
 copy and paste command below to set the exec permission to the script, create log file and setup crontab ; and install prerequises for node.js
 
 ```
@@ -34,6 +37,7 @@ sudo apt-get install -y nodejs libatk1.0-0 libatk-bridge2.0-0 libxdamage1 libgbm
 chmod +x test_flux.sh && mkdir crontab_logs & touch crontab_logs/test_flux.log && crontab -l | sed "\$a*/15 * * * * /home/$USER/test_flux.sh >> /home/$USER/crontab_logs/test_flux.log 2>&1" | crontab -
 
 ```
+
 the Crontab is set to execute script every 15 minutes
 
 Logs directory `/home/$USER/crontab_logs/test_flux.log`
--- test_flux.sh
+++ test_flux.sh
@@ -1,5 +1,8 @@
-PORT=$(hostname | awk '{print substr($0,length($0))}') #you can specify the port used, from 2 to 9, ie: PORT=5, meaning 16156 port
-WEBPAGE="http://127.0.0.1:161${PORT}6"
+#!/bin/bash
+APIPORT=$(fluxbench-cli getbenchmarks | jq -r '.ipaddress | split(":") | .[1]')
+UIPORT=$((APIPORT - 1))
+UIWEBPAGE="http://127.0.0.1:${UIPORT}"
+APIWEBPAGE="http://127.0.0.1:${APIPORT}"
 
 func_timestamp () {
     date +"%Y-%m-%d %T"
@@ -8,7 +11,7 @@ func_httpcode () {
 	HTTPCODE=$(curl --max-time 5 --silent --write-out %{response_code} --output "/dev/null" "$WEBPAGE")
     }
 func_upnpstatus () {
-    UPNPSTATUS=$(node puppeteer_upnp.js $WEBPAGE/benchmark/fluxnode/getbenchmarks | grep -c "UPNP")
+    UPNPSTATUS=$(node puppeteer_upnp.js $UIWEBPAGE/benchmark/fluxnode/getbenchmarks | grep -c "UPNP")
 }
 
 func_httpcode
@@ -36,3 +39,5 @@ else
         exit 0
     fi
 fi
+
+#todo: check the ui page with curl to http://127.0.0.1:${UIPORT} to get HTTPCODE. Then replace func_upnpstatus with curl to http://127.0.0.1:${APIPORT}/daemon/getbenchmarks, actual output : {"status":"success","data":"{\n  \"ipaddress\": \"86.66.168.9:16157\",\n  \"architecture\": \"amd64\",\n  \"armboard\": \"\",\n  \"status\": \"CUMULUS\",\n  \"time\": 1770312885,\n  \"cores\": 4,\n  \"ram\": 7.8,\n  \"ssd\": 222.99,\n  \"hdd\": 0,\n  \"ddwrite\": 432.37,\n  \"totalstorage\": 222.99,\n  \"disksinfo\": [\n    {\n      \"disk\": \"sda\",\n      \"size\": 222.99,\n      \"writespeed\": 432.37\n    }\n  ],\n  \"eps\": 391.36,\n  \"eps_singlethread\": 95.79000000000001,\n  \"eps_multithread\": 375.52,\n  \"ping\": 12.046,\n  \"download_speed\": 883.1046783125,\n  \"upload_speed\": 202.0023305,\n  \"bench_version\": \"1.0.20\",\n  \"speed_version\": \"1.2.0\",\n  \"systemsecure\": false,\n  \"error\": \"\"\n}\n"}. A working status = \"status\": \"CUMULUS\"
